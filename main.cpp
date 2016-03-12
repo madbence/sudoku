12697c9e56b25f4ac897a3d5851748df1a93d146
Bence Dányi <bence@danyi.me>
2016-03-12 17:41:29 +0100

    feat: improve solving algo

--- main.cpp
+++ main.cpp
@@ -1,20 +1,32 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
 
 const int N = 3;
 const int M = N * N;
+pthread_mutex_t p = PTHREAD_MUTEX_INITIALIZER;
 struct Sudoku {
   int data[M * M];
+  unsigned long long mask[M * M];
   int current;
   Sudoku() {}
   Sudoku(const char* c, int i = 0) {
     for (int i = 0; i < M * M; i++) {
-      data[i] = c[i] - '0';
+      data[i] = 0;
+      mask[i] = 0;
+      for (int j = 0; j < M; j++) {
+        mask[i] |= 1 << j;
+      }
+    }
+    for (int i = 0; i < M * M; i++) {
+      if (c[i] != '0') set(i, c[i] - '0', "init");
     }
     current = i;
   }
-  bool allowed(int v, int i, bool rec = true) {
+  bool allowed(int v, int i) {
+    return mask[i] & (1 << (v - 1));
     int x = i % M;
     int y = i / M;
     int bx = x / N * N;
@@ -35,20 +47,103 @@ struct Sudoku {
     }
     return true;
   }
+  bool solvable() {
+    for (int i = 0; i < M * M; i++) {
+      if (!mask[i]) {
+        /* printf("not solvable at %d (%d, %d)\n", i, i % M, i / M); */
+        /* print(); */
+        return false;
+      }
+    }
+    return true;
+  }
+  bool set(int i, int v, const char* phase) {
+    unsigned long long m = ~(1 << (v - 1));
+    int x = i % M;
+    int y = i / M;
+    int bx = x / N * N;
+    int by = y / N * N;
+    if (SLOW) {
+      printf("\033[%d;1H\033[2KSet (%d, %d) to %d in phase %s\n\n", (N + 1) * N + 4, x + 1, y + 1, v, phase);
+      print(i);
+      print_b();
+      usleep(1000 * SLOW);
+    }
+    data[i] = v;
+    for (int j = 0; j < M; j++) {
+      int ox = j % N;
+      int oy = j / N;
+      if (i != y * M + j) mask[y * M + j] &= m;
+      if (i != j * M + x) mask[j * M + x] &= m;
+      if (i != (by + oy) * M + bx + ox) mask[(by + oy) * M + bx + ox] &= m;
+      if (!mask[y * M + j] || !mask[j * M + x] || !mask[(by + oy) * M + bx + ox]) {
+        return false;
+      }
+    }
+    mask[i] = ~m;
+    return true;
+  }
   bool backtrack(Sudoku* w, int* n);
-  void print() {
+  bool eliminate();
+  bool loner();
+  void print(int i = -1) {
+    if (SLOW) {
+      print_(i);
+      return;
+    }
     for (int i = 0; i < M * M; i++) {
       printf("%d", data[i]);
     }
   }
+  void print_(int i) {
+    for (int y = 0; y < M; y++) {
+      for (int x = 0; x < M; x++) {
+        int j = y * M + x;
+        int c = data[j];
+        printf("%c", i == j ? '<' : (current == j ? '[' : ' '));
+        if (!c) printf(".");
+        else printf("%d", c);
+        printf("%c", i == j ? '>' : (current == j ? ']' : ' '));
+        if (x % N == N - 1) printf(" ");
+      }
+      printf("\n");
+      if (y % N == N - 1) printf("\n");
+    }
+    printf("\n");
+  }
+  void print_b() {
+    for (int y = 0; y < M; y++) {
+      for (int x = 0; x < M; x++) {
+        int c = data[y * M + x];
+        if (current == y * M + x) printf("["); else printf(" ");
+        if (!mask[y * M + x]) {
+          for (int i = M - 1; i >= 0; i--) {
+            printf("X");
+          }
+        } else if (c) {
+          for (int i = M - 1; i >= 0; i--) {
+            printf("%c", c == (i + 1) ? 'O' : ' ');
+          }
+        } else {
+          for (int i = M - 1; i >= 0; i--) {
+            bool allow = mask[y * M + x] >> i & 1;
+            printf("%c", allow ? '.' : ' ');
+          }
+        }
+        if (current == y * M + x) printf("]"); else printf(" ");
+        if (x % N == N - 1) printf(" ");
+      }
+      printf("\n");
+      if (y % N == N - 1) printf("\n");
+    }
+    printf("\n");
+  }
 };
 
-Sudoku stack[10000];
+Sudoku stack[100000];
 int curr = -1;
 int waiting = 0;
-const int T = 4;
-const int W = 100;
-int pushes = 0;
+unsigned long long pushes = 0;
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t wait = PTHREAD_MUTEX_INITIALIZER;
@@ -90,14 +185,109 @@ void push(Sudoku* w, int n) {
   pthread_mutex_unlock(&mutex);
 }
 
+bool Sudoku::loner() {
+  bool again;
+  do {
+    if(!eliminate()) return false;
+    again = false;
+    for (int y = 0; y < M; y++) {
+      int h[M] = {};
+      int p[M] = {};
+      for (int x = 0; x < M; x++) {
+        for (int i = 0; i < M; i++) {
+          int c = (mask[y * M + x] >> i) & 1;
+          h[i] += c;
+          if (c) p[i] = x;
+        }
+      }
+      for (int i = 0; i < M; i++) {
+        if (h[i] == 1 && !data[y * M + p[i]]) {
+          bool ok = set(y * M + p[i], i + 1, "hidden single in row");
+          if (!ok) return false;
+          again = true;
+          break;
+        }
+      }
+    }
+    for (int x = 0; x < M; x++) {
+      int h[M] = {};
+      int p[M] = {};
+      for (int y = 0; y < M; y++) {
+        for (int i = 0; i < M; i++) {
+          int c = (mask[y * M + x] >> i) & 1;
+          h[i] += c;
+          if (c) p[i] = y;
+        }
+      }
+      for (int i = 0; i < M; i++) {
+        if (h[i] == 1 && !data[p[i] * M + x]) {
+          bool ok = set(p[i] * M + x, i + 1, "hidden single in col");
+          if (!ok) return false;
+          again = true;
+          break;
+        }
+      }
+    }
+    for (int i = 0; i < M; i++) {
+      int h[M] = {};
+      int p[M] = {};
+      int bx = i % 3 * 3;
+      int by = i / 3 * 3;
+      for (int j = 0; j < M; j++) {
+        int x = bx + j % 3;
+        int y = by + j / 3;
+        for (int k = 0; k < M; k++) {
+          int c = (mask[y * M + x] >> k) & 1;
+          h[k] += c;
+          if (c) p[k] = j;
+        }
+      }
+      for (int k = 0; k < M; k++) {
+        int x = bx + p[k] % 3;
+        int y = by + p[k] / 3;
+        if (h[k] == 1 && !data[y * M + x]) {
+          bool ok = set(y * M + x, k + 1, "hidden single in block");
+          if (!ok) return false;
+          again = true;
+          break;
+        }
+      }
+    }
+  } while (again);
+  return true;
+}
+
+bool Sudoku::eliminate() {
+  bool again;
+  do {
+    again = false;
+    for (int i = 0; i < M * M; i++) {
+      for (int j = 0; j < M; j++) {
+        if (data[i] || mask[i] >> j > 1) break;
+        else if (!data[i]) {
+          bool ok = set(i, j + 1, "naked single");
+          if (!ok) return false;
+          again = true;
+          break;
+        }
+      }
+    }
+  }
+  while (again);
+  return true;
+}
+
 bool Sudoku::backtrack(Sudoku* w, int* n) {
   int i = 0;
+  if (!solvable()) return false;
+  if (!loner()) return false;
+  if (!solvable()) return false;
   while (current < M * M && data[current]) current++;
   if (current >= M * M) return true;
   for (int j = 1; j <= M; j++) {
     if (!allowed(j, current)) continue;
     Sudoku tmp = *this;
-    tmp.data[current] = j;
+    tmp.set(current, j, "backtrace");
     tmp.current++;
     w[i++] = tmp;
   }
@@ -105,7 +295,6 @@ bool Sudoku::backtrack(Sudoku* w, int* n) {
   return false;
 }
 
-pthread_mutex_t p = PTHREAD_MUTEX_INITIALIZER;
 
 void* worker(void* data) {
   int id = *(int*)data;
@@ -118,10 +307,12 @@ void* worker(void* data) {
     for (int i = 0; i < n; i++) {
       int k = 0;
       if (w[i].backtrack(&w_n[j], &k)) {
-        pthread_mutex_lock(&p);
-        w[i].print();
-        printf(" found by worker %d (after %d pushes to the stack)\n", id, pushes);
-        pthread_mutex_unlock(&p);
+        if (SLOW) {
+          printf("\033[16;1H");
+          printf("\033[2KSolution:\n\n");
+          w[i].print(); printf("\n");
+          usleep(1000 * 1000);
+        }
       }
       j += k;
     }
@@ -131,32 +322,46 @@ void* worker(void* data) {
 
 void solve(Sudoku s) {
   stack[curr = pushes = 0] = s;
-  // printf("searching for solutions on %d worker threads (workload size: %d)...\n", T, W);
-  stack[0].print();
-  printf(" is the problem\n");
-  pthread_cond_signal(&cond);
   pthread_mutex_lock(&d);
-  pthread_cond_wait(&done, &d);
+  pthread_mutex_lock(&mutex);
+  pthread_cond_signal(&cond);
+  pthread_cond_wait(&done, &mutex);
+  pthread_mutex_unlock(&mutex);
   pthread_mutex_unlock(&d);
-  // printf("no more solutions (job stack is empty, %d items were processed)\n\n", pushes);
 }
 
 int main() {
+  srand(time(NULL));
   int ids[T];
+  setbuf(stdout, NULL);
   pthread_t threads[T];
   for (int i = 0; i < T; i++) {
     ids[i] = i;
     int s = pthread_create(&threads[i], NULL, worker, (void*)&ids[i]);
   }
 
-  // Sudoku s("000801000000000043500000000000070800020030000000000100600000075003400000000200600");
-  // solve(s);
+  Sudoku s;
+  for (int i = 0; i < M * M; i++) {
+    s.data[i] = 0;
+    s.mask[i] = 0;
+    for (int j = 0; j < M; j++) {
+      s.mask[i] |= 1 << j;
+    }
+  }
 
   FILE* f = fopen("./puzzles.txt", "r");
   char buf[100] = {};
+  int i = 0;
   while (fgets(buf, 100, f) && !feof(f)) {
+    if (SLOW) printf("\033[2J\033[1;1HNext puzzle: %s", buf);
+    if (SLOW) usleep(1000 * 1000);
+    if (SLOW) printf("\033[2J\033[1;1HPuzzle:\n\nInit...");
     Sudoku s(buf);
+    if (SLOW) printf("\033[1;1HPuzzle:\n\n");
+    if (SLOW) s.print();
     solve(s);
+    if (i++ > 5000) return 0;
+    printf("%d\r", i);
   }
 
   return 0;
--- readme.markdown
+++ readme.markdown
@@ -1,28 +1,31 @@
 # parallel sudoku solver
 
+> parallel sudoku solver with `pthread`, uses backtracing as a last resort method,
+  otherwise it tries to solve the puzzle like a human would do.
+
 ## compile
 
 ```sh
-$ g++ -pthread -std=c++11 -o sudoku main.cpp
+$ g++ -pthread -std=c++11 -O0 -DT=4 -DW=1 -DSLOW=100 -o sudoku main.cpp
+# T=4: use 4 worker threads
+# W=1: workers will take 1 job per turn
+# SLOW=100: show progress, sleep 100 ms between steps (SLOW=0 to disable progress display)
 ```
 
 ## run
 
 ```sh
 $ curl http://staffhome.ecm.uwa.edu.au/~00013890/sudoku17 > puzzles.txt
-$ ./sudoku
+$ ./sudoku # will solve the first 5000 puzzles
 ```
 
 ## features
 
-- it finds *every* solution
+- it finds *every* solution (0, 1 or more)
 - can solve *9x9*, *16x16*, *25x25*, etc sizes
-
-## future
-
 - humanistic approach to improve speed
-  - [ ] naked single
-  - [ ] hidden single
+  - naked single (there is only one candidate for a cell)
+  - hidden single (there are multiple candidates for a cell, but one of them appears only once in a row/col/block)
 
 ## license
 
